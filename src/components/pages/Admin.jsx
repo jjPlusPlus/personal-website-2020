fcf236099b739067d9d31c9e9a1c1518f242052d
JJ <justinjcode@gmail.com>
2019-08-02 01:12:46 -0700

    spin up a pretty run of the mill admin sign in page

--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Admin = () => {
-  return (
-    <div className="admin">
-      <h2>This is my website</h2>
-      <p>lorem ipsum</p>
-    </div>
-  );
+class Admin extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      password: ""
+    }
+    this.authenticate = this.authenticate.bind(this)
+  }
+
+  inputChange = field => event => {
+    this.setState({
+      [field]: event.target.value
+    })
+  }
+
+  authenticate(event) {
+    event.preventDefault();
+
+  }
+
+  render() {
+    return (
+      <div className="admin">
+        <h2>This is my website and I want to change shit</h2>
+        <p>Prove it.</p>
+        <form onSubmit={this.authenticate}>
+          <input type="text" value={this.state.email} onChange={this.inputChange('email')} />
+          <input type="password" value={this.state.password} onChange={this.inputChange('password')} />
+          <button type="submit"> lets do eit. </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Admin;
