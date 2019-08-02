2c3cb3cfa21f33ff8f8630ab016d9e42512ea9b3
JJ <justinjcode@gmail.com>
2019-08-02 09:06:28 -0700

    begin the reduxification of the admin page

--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class Admin extends Component {
   constructor(props) {
@@ -18,22 +20,39 @@ class Admin extends Component {
 
   authenticate(event) {
     event.preventDefault();
-
+    const credentials = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    this.props.authenticate(credentials);
   }
 
   render() {
+    const { authenticating, user, authError } = this.props;
+
     return (
-      <div className="admin">
+      <div className="admin-page">
         <h2>This is my website and I want to change shit</h2>
-        <p>Prove it.</p>
-        <form onSubmit={this.authenticate}>
-          <input type="text" value={this.state.email} onChange={this.inputChange('email')} />
-          <input type="password" value={this.state.password} onChange={this.inputChange('password')} />
-          <button type="submit"> lets do eit. </button>
-        </form>
+        { user
+          ? <div className="admin-dashboard">
+              <strong>{user.email}</strong>
+            </div>
+          : <div className="sign-in-form">
+              <p>Prove it.</p>
+              <form onSubmit={this.authenticate}>
+                <input type="text" value={this.state.email} onChange={this.inputChange('email')} />
+                <input type="password" value={this.state.password} onChange={this.inputChange('password')} />
+                <button type="submit"> lets do eit. </button>
+              </form>
+            </div>
+          }
       </div>
     );
   }
 }
 
-export default Admin;
+const mapStateToProps = state => ({
+  ...state
+})
+
+export default connect(mapStateToProps, actions)(Admin);
