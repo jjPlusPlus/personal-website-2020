ee9717a64c5d32de000e48e18e9229f62a8781be
JJ <justinjcode@gmail.com>
2019-08-05 14:11:56 -0700

    Not very DRY, but for now copy projects/posts lists into editor list

--- src/components/admin/Posts.jsx
+++ src/components/admin/Posts.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import List from "../List";
+
+import { Link } from "react-router-dom";
+
+import * as actions from '../../actions';
+
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firebaseConnect } from 'react-redux-firebase';
 
 class Posts extends Component {
+
   render() {
+    const { posts } = this.props;
+    console.log(posts);
     return (
       <div className="posts">
-        <h2>Posts</h2>
+        <h1 className="page-header">My fucking articles</h1>
+
+        { posts &&
+          Object.keys(posts).map((post, index) => {
+            return (
+              <div className="post" key={index}>
+                <h3>{posts[post].name}</h3>
+                <p>{posts[post].description}</p>
+                <Link to = {{ pathname: `/admin/dashboard/posts/${post}`, state:{post: posts[post]}}} >Edit</Link>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
 }
-export default Posts;
+
+export default compose(
+  firebaseConnect(() => [
+    'posts'
+  ]),
+  connect((state) => ({
+    posts: state.firebase.data.posts,
+  }))
+)(Posts)
--- src/components/admin/Projects.jsx
+++ src/components/admin/Projects.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import List from "../List";
+
+import { Link } from "react-router-dom";
+
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firebaseConnect } from 'react-redux-firebase';
 
 class Projects extends Component {
+
   render() {
+    const { projects } = this.props;
     return (
       <div className="projects">
-        <h2>Projects</h2>
+        <h1 className="page-header">My fucking projects</h1>
+        { projects &&
+            Object.keys(projects).map((project, index) => {
+              return (
+                <div className="project" key={index}>
+                  <h3>{projects[project].name}</h3>
+                  <p>{projects[project].description}</p>
+                  <Link to = {{ pathname: `/admin/dashboard/projects/${project}`, state:{project: projects[project]}}}>Edit</Link>
+                </div>
+              )
+            })
+        }
       </div>
     )
   }
 }
-export default Projects;
+
+export default compose(
+  firebaseConnect(() => [
+    'projects'
+  ]),
+  connect((state) => ({
+    projects: state.firebase.data.projects,
+  }))
+)(Projects)
