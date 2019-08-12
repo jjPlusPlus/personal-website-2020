380841fce4dfde53c3f9452779e1f812ac4aabd3
JJ <justinjcode@gmail.com>
2019-08-12 11:52:57 -0700

    update the navigation page to include classNames & styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
-const Navigation = () => {
-  return (
-    <div className="navigation">
-      <h2>What do you want?</h2>
-      <Link to="/projects">I want to see JJ's fucking projects</Link>
-      <br />
-      <Link to="/posts">I want to read JJ's fucking articles</Link>
-      <br />
-      <Link to="/about">I want to know who the fuck JJ is</Link>
-      <br />
-      <Link to="/site">I want to know why this website is so fucking confusing</Link>
-      <br />
-      <Link to="/admin">I want to make fucking changes</Link>
-    </div>
-  );
+class Navigation extends Component {
+
+  render() {
+    return (
+      <div className="navigation-page">
+        <div className="navigation-page-gradient-cover"></div>
+        <div className="navigation">
+          <div className="navigation-page-header">
+            <h1 className="navigation-page-title">What do you want?</h1>
+          </div>
+          <Link to="/projects" className="navigation-item">
+            <h1>I want to see JJ's projects</h1>
+          </Link>
+          <Link to="/posts" className="navigation-item">
+            <h1>I want to read JJ's blog</h1>
+          </Link>
+          <Link to="/about" className="navigation-item">
+            <h1>I want to know more about JJ</h1>
+          </Link>
+          <Link to="/site" className="navigation-item">
+            <h1>This is a strange website</h1>
+          </Link>
+          <Link to="/admin" className="navigation-item">
+            <h1>I am JJ</h1>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Navigation;
