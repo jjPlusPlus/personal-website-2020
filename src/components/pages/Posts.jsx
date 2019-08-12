2ee1dabfde5c297aade6fd6ac1f3f657f738b0c9
JJ <justinjcode@gmail.com>
2019-08-12 11:54:31 -0700

    update the Posts page with queryparam  "tagKey" to show tags or posts

--- src/components/pages/Posts.jsx
+++ src/components/pages/Posts.jsx
@@ -9,35 +9,91 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firebaseConnect } from 'react-redux-firebase';
 
+import { encode, decode, addUrlProps, UrlQueryParamTypes, replaceInUrlQuery } from 'react-url-query';
+
 class Posts extends Component {
 
-  render() {
-    const { posts } = this.props;
-    return (
-      <div className="posts">
-        <h1 className="page-header">JJ's fucking articles:</h1>
+  constructor(props) {
+    super(props);
+    this.state = {
+      tag: null,
+    }
+    this.setTag = this.setTag.bind(this);
+  }
 
-        { posts &&
-          Object.keys(posts).map((post, index) => {
-            return (
-              <div className="post" key={index}>
-                <h3>{posts[post].name}</h3>
-                <p>{posts[post].description}</p>
-                <Link to = {{ pathname: `/posts/${post}`, state:{post: posts[post]}}} >More</Link>
+  setTag(key) {
+    replaceInUrlQuery('tagKey', key);
+  }
+
+  render() {
+    const { posts, tags, tagKey } = this.props;
+    const { tag } = this.state;
+
+    return (
+      <div className="posts-page">
+        <div className="posts">
+          <div className="posts-page--header">
+            { tags && tagKey
+              ? <h1 className="posts-page--title">Here are some posts about {tags[tagKey].name}</h1>
+              : <h1 className="posts-page--title">What kind of posts are you interested in?</h1>
+            }
+          </div>
+
+          { tags && !tagKey &&
+            Object.keys(tags).map((key, index) => {
+              const tag = tags[key];
+              if (tag.posts && tag.posts.length) {
+                return (
+                  <div onClick={() => this.setTag(key)} className="posts-page--tag" key={index}>
+                    <h1>I want to read posts about {tag.name}</h1>
+                  </div>
+                )
+              }
+            })
+          }
+
+          { posts && tagKey
+            ? <div className="posts-page--posts">
+                {
+                  Object.keys(posts).map((post, index) => {
+                    if (tagKey && posts[post].tags && posts[post].tags.includes(tagKey)) {
+                      return (
+                        <div className="posts-page--post" key={index}>
+                          <h1>{posts[post].name}</h1>
+                          <p>{posts[post].description}</p>
+                          <Link to = {{ pathname: `/posts/${post}`, state:{post: posts[post]}}} >More</Link>
+                        </div>
+                      )
+                    }
+                  })
+                }
+                <div onClick={() => this.setTag(null)} className="posts-page--unset-tag">
+                  <h1>I want to read about something else</h1>
+                </div>
               </div>
-            )
-          })
-        }
+            : null
+
+          }
+        </div>
       </div>
     )
   }
 }
 
+const urlPropsQueryConfig = {
+  tagKey: { type: UrlQueryParamTypes.string, queryParam: 'tagKey' }
+};
+
 export default compose(
   firebaseConnect(() => [
-    'posts'
+    'posts',
+    'tags'
   ]),
   connect((state) => ({
     posts: state.firebase.data.posts,
-  }))
+    tags: state.firebase.data.tags
+  })),
+  addUrlProps({
+    urlPropsQueryConfig
+  })
 )(Posts)
