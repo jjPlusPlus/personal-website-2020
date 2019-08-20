12638e49e5862908211594308840844e96c7a1fe
JJ <justinjcode@gmail.com>
2019-08-20 13:44:48 -0700

    Add the tagging functionality to the Projects page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -9,8 +9,21 @@ import { firebaseConnect } from 'react-redux-firebase';
 
 class Projects extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      tag: null,
+    }
+    this.setTag = this.setTag.bind(this);
+  }
+
+  setTag(key) {
+    replaceInUrlQuery('tagKey', key);
+  }
+
   render() {
-    const { projects } = this.props;
+    const { projects, tags, tagKey } = this.props;
+    const { tag } = this.state;
     return (
       <div className="projects">
         <h1 className="page-header">Some fucking projects by JJ</h1>
@@ -30,11 +43,20 @@ class Projects extends Component {
   }
 }
 
+const urlPropsQueryConfig = {
+  tagKey: { type: UrlQueryParamTypes.string, queryParam: 'tagKey' }
+};
+
 export default compose(
   firebaseConnect(() => [
-    'projects'
+    'projects',
+    'tags'
   ]),
   connect((state) => ({
     projects: state.firebase.data.projects,
-  }))
+    tags: state.firebase.data.tags
+  })),
+  addUrlProps({
+    urlPropsQueryConfig
+  })
 )(Projects)
