ca00e8400299b141e1b0608a49c819cf8bd8e4da
JJ <justinjcode@gmail.com>
2019-08-20 13:52:47 -0700

    Update the Navigation component to now match Posts & Projects

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,31 +1,60 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+
+import { CSSTransition } from 'react-transition-group';
+import DelayLink from './DelayLink';
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      delaying: false
+    }
+  }
+
+  delayStart = () => {
+    this.setState({
+      delaying: true
+    })
+  }
 
   render() {
+    const { delaying } = this.state;
     return (
-      <div className="navigation-page">
-        <div className="navigation-page-gradient-cover"></div>
-        <div className="navigation">
-          <div className="navigation-page-header">
-            <h1 className="navigation-page-title">What do you want?</h1>
+      <div className="page--container navigation-page ">
+        <div className="page">
+
+          <div className="page--header">
+            <CSSTransition appear={true} in={true} timeout={111} classNames="text-animation">
+              <h1 className="page--title">What do you want?</h1>
+            </CSSTransition>
           </div>
-          <Link to="/projects" className="navigation-item">
-            <h1>I want to see JJ's projects</h1>
-          </Link>
-          <Link to="/posts" className="navigation-item">
-            <h1>I want to read JJ's blog</h1>
-          </Link>
-          <Link to="/about" className="navigation-item">
-            <h1>I want to know more about JJ</h1>
-          </Link>
-          <Link to="/site" className="navigation-item">
-            <h1>This is a strange website</h1>
-          </Link>
-          <Link to="/admin" className="navigation-item">
-            <h1>I am JJ</h1>
-          </Link>
+          <div className={"fade-left " + (delaying === true ? "fading-left" : "")}>
+            <CSSTransition appear={true} in={true} timeout={111} classNames="list-animation">
+              <DelayLink delay={333} to="/projects" className="page-list-item" onDelayStart={() => this.delayStart()}>
+                <h1>I want to see JJ's projects</h1>
+              </DelayLink>
+            </CSSTransition>
+            <CSSTransition appear={true} in={true} timeout={222} classNames="list-animation">
+              <DelayLink delay={333} to="/posts" className="page-list-item" onDelayStart={() => this.delayStart()}>
+                <h1>I want to read JJ's blog</h1>
+              </DelayLink>
+            </CSSTransition>
+            <CSSTransition appear={true} in={true} timeout={333} classNames="list-animation">
+              <DelayLink delay={333} to="/about" className="page-list-item" onDelayStart={() => this.delayStart()}>
+                <h1>Who is JJ</h1>
+              </DelayLink>
+            </CSSTransition>
+            <CSSTransition appear={true} in={true} timeout={444} classNames="list-animation">
+              <DelayLink delay={333} to="/site" className="page-list-item" onDelayStart={() => this.delayStart()}>
+                <h1>This is a strange website</h1>
+              </DelayLink>
+            </CSSTransition>
+            <CSSTransition appear={true} in={true} timeout={555} classNames="list-animation">
+              <DelayLink delay={333} to="/admin" className="page-list-item" onDelayStart={() => this.delayStart()}>
+                <h1>I am JJ</h1>
+              </DelayLink>
+            </CSSTransition>
+          </div>
         </div>
       </div>
     );
