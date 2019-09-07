42df38d3f66e87a74a7591a39a9cd2a69324ab83
JJ <justinjcode@gmail.com>
2019-09-06 20:25:08 -0700

    Add a Sign out button to the dashboard

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -4,15 +4,21 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
 
 class Dashboard extends Component {
+  signOut() {
+    this.props.firebase.logout();
+  }
   render() {
     const profile = this.props.auth.email;
     return (
       <div className="admin-dashboard">
         <h1>Admin Dashboard</h1>
         { profile
-          ? <p>{profile}</p>
-          : <p>loading...</p>
-        }
+              ? <div className="flex flex-row flex-center">
+                  <p className="flex-1">{profile}</p>
+                  <button className="button" onClick={() => this.signOut()}>Sign out</button>
+                </div>
+              : <p>loading...</p>
+            }
 
         <Link to="/admin/dashboard/posts">I want to write some fucking articles</Link>
         <br />
@@ -23,11 +29,15 @@ class Dashboard extends Component {
     )
   }
 }
-const enhance = connect(
-  // Map redux state to component props
-  ({ firebase: { auth, profile } }) => ({
+
+const enhance = compose(
+  firebaseConnect([
+    'settings' // sync /todos from firebase into redux
+  ]),
+  connect(({firebase: { auth, profile }}) => ({
     auth,
     profile
-  })
-)
+  }))
+);
+
 export default enhance(Dashboard);
