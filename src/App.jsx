b54577a78e633f6721bd4f77c969cf45edc74d13
JJ <justinjcode@gmail.com>
2019-12-07 20:46:42 -0800

    rewrite the routing module with /v1 and /v2 routes , using a redirect

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import { play, exit } from "./animations";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-import LandingA from './components/LandingA';
-import LandingB from './components/LandingB';
+import V2 from './components/V2';
 
 import Navigation from './components/Navigation';
+
 import Posts from './components/pages/Posts';
 import PostDetail from './components/pages/PostDetail';
 import Projects from './components/pages/Projects';
 import ProjectDetail from './components/pages/ProjectDetail';
+
 import About from './components/pages/About';
 import Site from './components/pages/Site';
 import Admin from './components/pages/Admin';
 
-import SignInForm from './components/SignInForm';
 import Dashboard from './components/admin/Dashboard';
 import AdminPosts from './components/admin/Posts';
 import PostEditor from './components/admin/PostEditor';
@@ -50,14 +50,15 @@ class App extends Component {
               timeout={{enter: 333, exit: 333}}
             >
               <Switch location={location}>
-                <Route path="/" exact component={LandingB} />
-                <Route path="/app" exact component={Navigation} />
-                <Route exact path="/posts" component={Posts} />
-                  <Route path="/posts/:id" component={PostDetail} />
-                <Route exact path="/projects" component={Projects} />
-                  <Route path="/projects/:id" component={ProjectDetail} />
-                <Route path="/about" component={About} />
-                <Route path="/site" component={Site} />
+                <Route path="/v1" exact component={Navigation} />
+                <Route path="/v2" exact component={V2} />
+
+                <Route exact path="/v1/posts" component={Posts} />
+                  <Route path="/v1/posts/:id" component={PostDetail} />
+                <Route exact path="/v1/projects" component={Projects} />
+                  <Route path="/v1/projects/:id" component={ProjectDetail} />
+                <Route path="/v1/about" component={About} />
+                <Route path="/v1/site" component={Site} />
                 <Route exact path="/admin" component={Admin} />
                   <ProtectedRoute exact path="/admin/dashboard" component={Dashboard}/>
                     <ProtectedRoute exact path="/admin/dashboard/posts" component={AdminPosts} />
@@ -65,6 +66,7 @@ class App extends Component {
                     <ProtectedRoute exact path="/admin/dashboard/projects" component={AdminProjects} />
                       <ProtectedRoute path="/admin/dashboard/projects/:id" component={ProjectEditor} />
                     <ProtectedRoute path="/admin/dashboard/about" component={ResumeEditor} />
+                <Redirect to="/v2" />
               </Switch>
             </CSSTransition>
           </TransitionGroup>
