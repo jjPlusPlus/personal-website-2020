89e2a43d9b089ba55d71ca7c70ea321e1df148ce
JJ <justinjcode@gmail.com>
2020-03-20 00:57:02 -0700

    Remove the Resource Detail Modal from the home page entirely

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -90,31 +90,28 @@ class HomePage extends Component {
             </div>
           </div>
 
-          { detail ? (
-              <ResourceDetailModal tags={tags} detail={detail} />
-            ) : (
-            <div className={ "home-page " + (this.state.showMain ? "visible" : "hidden")}>
-              <section className={"header-display " + (animating ? "v2-header-animating" : "")}>
-                { selected ? (
+          <div className={ "home-page " + (this.state.showMain ? "visible" : "hidden")}>
+            <section className={"header-display " + (animating ? "v2-header-animating" : "")}>
+              { selected ? (
 
-                  <span>
-                    <Imgix
-                      src={selected.item.heroImage}
-                      sizes="(min-width: 1280px) 1280px, 100vw"
-                      imgixParams={{ ar: "5:2", auto: "format", fit: "crop" }}
-                      classNames="full-width"
-                    />
-                    <div className="header-display--content-wrapper flex flex-column">
-                      <div className="header-display--content">
-                        <h1>{selected.item.name}</h1>
-                        <p>{selected.item.snippet}</p>
-                        <div className="header-display--content-tags">
-                          { selected.item.tags && selected.item.tags.map((tag, index) => (
-                            <div className="tag" key={index} style={{ backgroundColor: tags[tag].bgColor || "#007aff", color: tags[tag].color || '#FFF' }}>
-                              <p>{tags[tag].name}</p>
-                            </div>
-                          ))}
-                        </div>
+                <span>
+                  <Imgix
+                    src={selected.item.heroImage}
+                    sizes="(min-width: 1280px) 1280px, 100vw"
+                    imgixParams={{ ar: "5:2", auto: "format", fit: "crop" }}
+                    classNames="full-width"
+                  />
+                  <div className="header-display--content-wrapper flex flex-column">
+                    <div className="header-display--content">
+                      <h1>{selected.item.name}</h1>
+                      <p>{selected.item.snippet}</p>
+                      <div className="header-display--content-tags">
+                        { selected.item.tags && selected.item.tags.map((tag, index) => (
+                          <div className="tag" key={index} style={{ backgroundColor: tags[tag].bgColor || "#007aff", color: tags[tag].color || '#FFF' }}>
+                            <p>{tags[tag].name}</p>
+                          </div>
+                        ))}
+                      </div>
                       <DelayLink 
                         delay={333} 
                         className="watch-now-button" 
@@ -128,45 +125,44 @@ class HomePage extends Component {
                       >
                         <p>WATCH NOW</p>
                       </DelayLink>
-                      </div>
                     </div>
-                  </span>
-                ) : (
-                  <span>
-                    {/* <HeaderAnimation /> 
-                    <div className="image--aspect-wrapper--16-9" style={{ backgroundImage: "url(/jjpp-header-slim.svg)" }}></div>*/}
-                    <Imgix
-                      src="https://jj-plus-plus.imgix.net/images/jjpp-header-slim.svg"
-                      sizes="(min-width: 1280px) 1280px, 100vw"
-                      imgixParams={{ ar: "5:2", auto: "format", fit: "crop" }}
-                      classNames="full-width"
-                    />
-                    <h1 className="header-display--title">This is JJ</h1>
-                  </span>
-                )
-                }
-              </section>
+                  </div>
+                </span>
+              ) : (
+                <span>
+                  {/* <HeaderAnimation /> 
+                  <div className="image--aspect-wrapper--16-9" style={{ backgroundImage: "url(/jjpp-header-slim.svg)" }}></div>*/}
+                  <Imgix
+                    src="https://jj-plus-plus.imgix.net/images/jjpp-header-slim.svg"
+                    sizes="(min-width: 1280px) 1280px, 100vw"
+                    imgixParams={{ ar: "5:2", auto: "format", fit: "crop" }}
+                    classNames="full-width"
+                  />
+                  <h1 className="header-display--title">This is JJ</h1>
+                </span>
+              )
+              }
+            </section>
 
-              <section className="projects-display">
-                <h1 className="section-title"> <StringGlitch interval={3000} text="PROJECTS" /> </h1>
-                {projects && projects.length ? (
-                  <HomeSlider items={projects} resource="projects" selected={selected} setSelected={(item, type) => this.delaySetSelected(item, type)}/>
-                ) : null }
-              </section>
+            <section className="projects-display">
+              <h1 className="section-title"> <StringGlitch interval={3000} text="PROJECTS" /> </h1>
+              {projects && projects.length ? (
+                <HomeSlider items={projects} resource="projects" selected={selected} setSelected={(item, type) => this.delaySetSelected(item, type)}/>
+              ) : null }
+            </section>
 
-              <section className="posts-display">
-                <h1 className="section-title"><StringGlitch interval={4000} text="POSTS" /></h1>
-                {posts && posts.length ? (
-                  <HomeSlider items={posts} resource="posts" selected={selected} setSelected={(item, type) => this.delaySetSelected(item, type)}/>
-                ) : null}
-              </section>
+            <section className="posts-display">
+              <h1 className="section-title"><StringGlitch interval={4000} text="POSTS" /></h1>
+              {posts && posts.length ? (
+                <HomeSlider items={posts} resource="posts" selected={selected} setSelected={(item, type) => this.delaySetSelected(item, type)}/>
+              ) : null}
+            </section>
 
-              <section className="about">
-                <h1 className="section-title">WHO IS JJ</h1>
-                <About />
-              </section>
-            </div>
-          )}
+            <section className="about">
+              <h1 className="section-title">WHO IS JJ</h1>
+              <About />
+            </section>
+          </div>
         </div>
 
         <section className="footer-wrapper flex-center">
